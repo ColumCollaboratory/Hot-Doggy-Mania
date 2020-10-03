04bfc3c23d916e766c93522c7879454e0b56a0bd
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-10-03 10:40:40 -0500

    implements audio singleton
    exposes clips so they can be easily played by other scripts

---
+++ Assets/Audio/AudioSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class AudioSingleton : MonoBehaviour
+{
+    public static AudioSingleton instance;
+
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private string[] clipNames;
+
+    private Dictionary<string, AudioClip> clips;
+
+    private void Start()
+    {
+        instance = this;
+        if (audioClips.Length == clipNames.Length)
+        {
+            clips = new Dictionary<string, AudioClip>();
+            for (int i = 0; i < clipNames.Length; i++)
+                clips.Add(clipNames[i], audioClips[i]);
+        }
+        bgmSource.loop = true;
+    }
+
+    public void PlaySFX(string clipName)
+    {
+        sfxSource.PlayOneShot(clips[clipName]);
+    }
+    public void PlayBGM(string clipName)
+    {
+        StopBGM();
+        bgmSource.clip = clips[clipName];
+        bgmSource.Play();
+    }
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+}
