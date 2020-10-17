571818727bb0398862d1fc40c3ea0f13cff5c313
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2020-10-16 23:55:01 -0400

    Update ui in scripts.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -8,6 +9,12 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     [Tooltip("How long the enemy is downed for")]
     private float downTimer=5;
+    [SerializeField]
+    private GameObject lifeOne;
+    [SerializeField]
+    private GameObject lifeTwo;
+    [SerializeField]
+    private GameObject lifeThree;
     bool canKill = true;
     //Prevents enemy dying while already dead
     bool canDie = true;
@@ -33,11 +40,21 @@ public class Enemy : MonoBehaviour
             {
                 PlayerLives.Lives = PlayerLives.Lives - 1;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                if (PlayerLives.Lives == 2)
+                {
+                    lifeThree.SetActive(false);
+                }
+                else if (PlayerLives.Lives == 1)
+                {
+                    lifeTwo.SetActive(false);
+                }
             }
             else
             {
                 PlayerLives.Lives = 3;
                 SceneManager.LoadScene(0);
+                lifeTwo.SetActive(true);
+                lifeThree.SetActive(true);
                 AudioSingleton.instance?.PlayBGM("Menu_BGM");
             }
         }
@@ -46,6 +63,7 @@ public class Enemy : MonoBehaviour
             if(collision.GetComponent<Ingredients>().GetFalling()==true&&canDie==true)
             {
                 AudioSingleton.instance?.PlaySFX("Squish_Enemy");
+                Score.score += 20;
                 Down();
             }
         }
---
+++ Assets/Scripts/ResourceManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ResourceManagement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lifeOne;
+    [SerializeField]
+    private GameObject lifeTwo;
+    [SerializeField]
+    private GameObject lifeThree;
+    [SerializeField]
+    private Text scoreCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerLives.Lives == 2)
+        {
+            lifeThree.SetActive(false);
+        }
+        else if (PlayerLives.Lives == 1)
+        {
+            lifeThree.SetActive(false);
+            lifeTwo.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCounter.text = Score.score.ToString();
+    }
+}
