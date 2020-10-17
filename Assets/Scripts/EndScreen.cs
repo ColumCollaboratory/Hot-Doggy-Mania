2104c7689b17dfc5751777b50458a2b8e5026ce4
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-10-16 23:09:14 -0500

    impleemnts end screen

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    private void Start()
+    {
+        AudioSingleton.instance?.PlayBGM("Menu_BGM");
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+}
